ab420345023cd87d145d75f78ccca55867f3d2d0
Antonio Luis <antonioluiscliment@gmail.com>
2013-07-07 20:15:10 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2! \n Hello Fini!');
+  response.send(f.readFileSync(index.htlm).toString);
 });
 
 var port = process.env.PORT || 5000;
