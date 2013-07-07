8befe8073190baccce826d5d57223ef64bb91dd6
Antonio Luis <antonioluiscliment@gmail.com>
2013-07-07 20:29:55 +0000

    nth

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(" Hello f.readFileSync(lm").toString");
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
