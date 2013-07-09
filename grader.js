8c9be6d8b8e20f8068287bcb266cc286d9224ffa
antonioluiscliment <antonioluiscliment@gmail.com>
2013-07-09 21:29:45 +0000

    grader wk3 part 3

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'http://github.com/antonioluiscliment/bitstarter/index.html';
+var inUrl = "";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +38,17 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var responseUrl = function(result) {
+    //   console.log(result);
+    return result;
+};
+
+var assertUrlExists = function(infile) {
+    //    console.log(infile);
+    return rest.get(infile).on('complete', responseUrl);
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,6 +79,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url index.html', clone(assertUrlExists), URL_DEFAULT) 
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
